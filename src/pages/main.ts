77d01f9401b679d1e7583e4febf72013202ec69f
Souchy <robyn.g@hotmail.fr>
2025-07-21 17:31:03 -0400

    main.ts register all SouchyAu components

--- src/pages/main.ts
+++ src/pages/main.ts
@@ -1,20 +1,13 @@
 import Aurelia from 'aurelia';
 import { RouterConfiguration } from '@aurelia/router';
 import { MyApp } from './my-app';
-import { ModalTargetCustomAttribute } from '../components/modal/modal-target';
-import { Splitgrid } from '../components/splitgrid/splitgrid';
-import { Modal } from '../components/modal/modal';
-import { FillCustomAttribute } from '../components/button/fill';
-import { ShapeCustomAttribute } from '../components/button/shape';
-import { ToggleableCustomAttribute } from '../components/button/toggleable';
-import { RadioGroupCustomAttribute } from '../components/button/radiogroup';
+import * as SouchyAu from '../index';
 
 Aurelia
   .register(RouterConfiguration)
   // To use HTML5 pushState routes, replace previous line with the following
   // customized router config.
   // .register(RouterConfiguration.customize({ useUrlFragmentHash: false }))
-  .register(ModalTargetCustomAttribute, FillCustomAttribute, ShapeCustomAttribute, ToggleableCustomAttribute, RadioGroupCustomAttribute)
-  .register(Splitgrid, Modal)
+  .register(SouchyAu)
   .app(MyApp)
   .start();
