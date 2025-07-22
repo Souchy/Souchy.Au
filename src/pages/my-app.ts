1b8bedafe63abf3f1fa61c0fcfc7b3f0995379ea
Souchy <robyn.g@hotmail.fr>
2025-07-22 00:47:02 -0400

    some cleanup

--- src/pages/my-app.ts
+++ src/pages/my-app.ts
@@ -1,6 +1,6 @@
-import { IRouter, ICurrentRoute, INavigationModel, IRouteContext, IRouterEvents, LocationChangeEvent, NavigationCancelEvent, NavigationEndEvent, NavigationErrorEvent, NavigationStartEvent, ParameterInformation, route } from '@aurelia/router';
+import { IRouter, ICurrentRoute, IRouterEvents, NavigationEndEvent, route } from '@aurelia/router';
 import { foundRoutes, PageClass, foundSidebars } from './routes';
-import { EventAggregator, IDisposable, IEventAggregator, inject, lazy, resolve } from 'aurelia';
+import { EventAggregator, IDisposable, inject } from 'aurelia';
 
 @route({
   routes: [
@@ -16,29 +16,11 @@ import { EventAggregator, IDisposable, IEventAggregator, inject, lazy, resolve }
 @inject(EventAggregator, IRouterEvents, ICurrentRoute, IRouter)
 export class MyApp implements IDisposable {
   private sidebar: PageClass | undefined = undefined;
-  // private readonly nav_model: INavigationModel = resolve(IRouteContext).routeConfigContext.navigationModel;
-  // private readonly routeContext = resolve(lazy(IRouteContext));
   private readonly subscriptions: IDisposable[];
 
-  constructor(private ea: EventAggregator, events: IRouterEvents, private currentRoute: ICurrentRoute) {
-    console.log("ctor curr route: ", currentRoute);
-
-    // this.ea.subscribe("au:router:navigation-end", () => this.onNavEnd);
-    // const events = resolve(IRouterEvents);
+  constructor(events: IRouterEvents, private currentRoute: ICurrentRoute) {
     this.subscriptions = [
-      // events.subscribe('au:router:location-change', (event: LocationChangeEvent) => {
-      //   console.log("on nav change", event);
-      // }),
-      // events.subscribe('au:router:navigation-start', (event: NavigationStartEvent) => {
-      //   console.log("on nav start", event);
-      // }),
-      events.subscribe('au:router:navigation-end', (event: NavigationEndEvent) => this.onNavEnd(event)),
-      // events.subscribe('au:router:navigation-cancel', (event: NavigationCancelEvent) => {
-      //   console.log("on nav cancel", event);
-      // }),
-      // events.subscribe('au:router:navigation-error', (event: NavigationErrorEvent) => {
-      //   console.log("on nav error", event);
-      // }),
+      events.subscribe('au:router:navigation-end', this.onNavEnd.bind(this)),
     ];
   }
 
@@ -56,14 +38,8 @@ export class MyApp implements IDisposable {
   }
 
   private onNavEnd(event: NavigationEndEvent): void {
+    console.log("nav curr route: ", this.currentRoute);
     this.sidebar = foundSidebars.get(this.currentRoute.title);
-    // console.log("on nav end", event);
-    // console.log(`-currentRoute: `, this.currentRoute);
-    // console.log(`-path: ${this.currentRoute.path}`);
-    // console.log(`-url: ${this.currentRoute.url}`);
-    // console.log(`-title: ${this.currentRoute.title}`);
-    // console.log(`-query: ${this.currentRoute.query}`);
-    // console.log(`-sidebar: `, this.sidebar);
   }
 
 }
--- src/pages/welcome-page/welcome-side.ts
+++ src/pages/welcome-page/welcome-side.ts
@@ -5,15 +5,15 @@ export class WelcomeSide {
 	@bindable text: string = "Welcome to Souchy.Au!";
 	
 	constructor() {
-		console.log("WelcomeSide component ctor.");
+		// console.log("WelcomeSide component ctor.");
 	}
 
 	public attached(): void {
-		console.log("WelcomeSide component attached.");
+		// console.log("WelcomeSide component attached.");
 	}
 
 	public binding(): void {
-		console.log("WelcomeSide component binding.");
+		// console.log("WelcomeSide component binding.");
 	}
 
 }
