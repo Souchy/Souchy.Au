d37abcdbe60b4315750ad25210fc74bb1ef8534a
Souchy <robyn.g@hotmail.fr>
2025-07-22 00:37:14 -0400

    routes + sidebar component work

--- src/pages/my-app.ts
+++ src/pages/my-app.ts
@@ -1,49 +1,49 @@
 import { IRouter, ICurrentRoute, INavigationModel, IRouteContext, IRouterEvents, LocationChangeEvent, NavigationCancelEvent, NavigationEndEvent, NavigationErrorEvent, NavigationStartEvent, ParameterInformation, route } from '@aurelia/router';
-import { routes, PageClass } from './routes';
+import { foundRoutes, PageClass, foundSidebars } from './routes';
 import { EventAggregator, IDisposable, IEventAggregator, inject, lazy, resolve } from 'aurelia';
 
 @route({
-  routes: routes,
+  routes: [
+    {
+      path: [''],
+      component: import('./welcome-page/welcome-page'),
+      title: 'Welcome',
+    },
+    ...foundRoutes
+  ],
   fallback: import('./missing-page/missing-page'),
 })
 @inject(EventAggregator, IRouterEvents, ICurrentRoute, IRouter)
 export class MyApp implements IDisposable {
-  // private readonly currentRoute: ICurrentRoute = resolve(ICurrentRoute);
-  private sidebar: PageClass;
-  private routess = routes;
+  private sidebar: PageClass | undefined = undefined;
   // private readonly nav_model: INavigationModel = resolve(IRouteContext).routeConfigContext.navigationModel;
-  private readonly routeContext = resolve(lazy(IRouteContext));
-
+  // private readonly routeContext = resolve(lazy(IRouteContext));
   private readonly subscriptions: IDisposable[];
-  public log: string[] = [];
 
   constructor(private ea: EventAggregator, events: IRouterEvents, private currentRoute: ICurrentRoute) {
     console.log("ctor curr route: ", currentRoute);
-    
+
     // this.ea.subscribe("au:router:navigation-end", () => this.onNavEnd);
     // const events = resolve(IRouterEvents);
     this.subscriptions = [
-      events.subscribe('au:router:location-change', (event: LocationChangeEvent) => {
-        console.log("on nav change", event);
-      }),
-      events.subscribe('au:router:navigation-start', (event: NavigationStartEvent) => {
-        console.log("on nav start", event);
-      }),
-      events.subscribe('au:router:navigation-end', (event: NavigationEndEvent) => {
-        console.log("on nav end", event);
-        console.log(`-currentRoute: `, this.currentRoute);
-        console.log(`-path: ${this.currentRoute.path}`);
-        console.log(`-url: ${this.currentRoute.url}`);
-        console.log(`-title: ${this.currentRoute.title}`);
-        console.log(`-query: ${this.currentRoute.query}`);
-      }),
-      events.subscribe('au:router:navigation-cancel', (event: NavigationCancelEvent) => {
-        console.log("on nav cancel", event);
-      }),
-      events.subscribe('au:router:navigation-error', (event: NavigationErrorEvent) => {
-        console.log("on nav error", event);
-      }),
+      // events.subscribe('au:router:location-change', (event: LocationChangeEvent) => {
+      //   console.log("on nav change", event);
+      // }),
+      // events.subscribe('au:router:navigation-start', (event: NavigationStartEvent) => {
+      //   console.log("on nav start", event);
+      // }),
+      events.subscribe('au:router:navigation-end', (event: NavigationEndEvent) => this.onNavEnd(event)),
+      // events.subscribe('au:router:navigation-cancel', (event: NavigationCancelEvent) => {
+      //   console.log("on nav cancel", event);
+      // }),
+      // events.subscribe('au:router:navigation-error', (event: NavigationErrorEvent) => {
+      //   console.log("on nav error", event);
+      // }),
     ];
+  }
+
+  public get routes() {
+    return foundRoutes;
   }
 
   public dispose(): void {
@@ -55,30 +55,15 @@ export class MyApp implements IDisposable {
     }
   }
 
-  // public async binding() {
-  //   await this.nav_model.resolve()
-  // }
-
-  public attached(): void {
-    console.log(`currentRoute: `, this.currentRoute);
-    console.log(`path: ${this.currentRoute.path}`);
-    console.log(`url: ${this.currentRoute.url}`);
-    console.log(`title: ${this.currentRoute.title}`);
-    console.log(`query: ${this.currentRoute.query}`);
-
-    for (const pi of this.currentRoute.parameterInformation) {
-      this.logParameterInstruction(pi);
-    }
+  private onNavEnd(event: NavigationEndEvent): void {
+    this.sidebar = foundSidebars.get(this.currentRoute.title);
+    // console.log("on nav end", event);
+    // console.log(`-currentRoute: `, this.currentRoute);
+    // console.log(`-path: ${this.currentRoute.path}`);
+    // console.log(`-url: ${this.currentRoute.url}`);
+    // console.log(`-title: ${this.currentRoute.title}`);
+    // console.log(`-query: ${this.currentRoute.query}`);
+    // console.log(`-sidebar: `, this.sidebar);
   }
-  private logParameterInstruction(pi: ParameterInformation): void {
-    console.log(`config: ${pi.config.id}`);
-    console.log(`viewport: ${pi.viewport}`);
-    console.log(`params:`, pi.params);
-    pi.children.forEach((c) => this.logParameterInstruction(c));
-  }
-
-  // private onNavEnd(ev) {
-  //   console.log("on nav end: ", ev)
-  // }
 
 }
--- src/pages/routes.ts
+++ src/pages/routes.ts
@@ -1,71 +1,64 @@
-import { IChildRouteConfig, IRouteConfig, Routeable } from "@aurelia/router";
+import { Routeable } from "@aurelia/router";
 
 export type PageClass = new (...args: any[]) => any;
-export interface Page extends IChildRouteConfig {
-	readonly sidebar?: Routeable;
-}
-
-const defaultPage = "welcome-page";
 
 // Eagerly import all page modules and HTML-only views
 // const modulePages = import.meta.glob('./*/!(*.stories).ts', { eager: true });
-const modulePages: Record<string, Record<string, any>> = import.meta.glob('./*/!(*.stories)!(*-side).ts', { eager: true });
 // const modulePages: Record<string, Record<string, any>> = import.meta.glob('./*-page.ts', { eager: true });
-const moduleSidebars: Record<string, Record<string, any>> = import.meta.glob('./*-side.ts', { eager: true });
-
+const modulePages: Record<string, Record<string, any>> = import.meta.glob('./*/!(*.stories)!(*-side).ts', { eager: true });
+const moduleSidebars: Record<string, Record<string, any>> = import.meta.glob('./**/*-side.ts', { eager: true });
+// console.log('Found sidebars:', moduleSidebars);
 // console.log(`Found ${Object.keys(modulePages)} page modules.`);
-export const routes: Routeable[] = [];
-export const sidebars = new Map<string, PageClass>();
+
+export const foundRoutes: Routeable[] = [];
+/** <route.title, sidebar component> */
+export const foundSidebars = new Map<string, PageClass>();
 
 function getComponent(path: string, mod: Record<string, any> | undefined) {
-	if(!mod) return undefined;
+	if (!mod) return undefined;
 	// Find the exported class
 	const exportKey = Object.keys(mod).find(k => /^[A-Z]/.test(k));
 	const component = exportKey ? mod[exportKey] : undefined;
-	// if (!component) continue;
 	return component;
 }
 
 for (const [path, mod] of Object.entries(modulePages)) {
-	if(path.endsWith("-side.ts")) continue;
+	if (path.endsWith("-side.ts")) continue;
 	// Find the exported class
-	// const exportKey = Object.keys(mod).find(k => /^[A-Z]/.test(k));
-	// const component: PageClass | undefined = exportKey ? mod[exportKey] : undefined;
-	// if (!component) continue;
 	const component = getComponent(path, mod);
 
 	// Extract the page folder name
 	let slashIndex = path.lastIndexOf('/');
+	let folder = path.substring(0, slashIndex);
 	let filename = path.substring(slashIndex + 1);
 	const name = filename.replace(/\.ts$/, '');
 	const nameWithoutPage = name.replace(/-page$/, '');
-	// console.log(`Found page: `, name);
+	// console.log(`Found page: `, path, folder, name);
 
 	// path
-	let routePath: string | string[] = name === defaultPage ? ['', nameWithoutPage] : nameWithoutPage;
+	// let routePath: string | string[] = name === defaultPage ? ['', nameWithoutPage] : nameWithoutPage;
+	let routePath: string | string[] = nameWithoutPage;
 
 	// title
-	let title =
-		nameWithoutPage
-			.replace(/-/g, ' ') // replace "-"" with space
-			.replace(/(^\w|\s\w)/g, l => l.toUpperCase()); // capitalize first letter of each word
+	let title = nameWithoutPage
+		.replace(/-/g, ' ') // replace "-"" with space
+		.replace(/(^\w|\s\w)/g, l => l.toUpperCase()); // capitalize first letter of each word
 
-
-	// const sidebarPath = `./sidebars/${nameWithoutPage}-sidebar.ts`;
-	const sidebarPath = path.replace("-page", "-side");
-	const sidebarMod = moduleSidebars[sidebarPath];
-	const sideComponent = getComponent(sidebarPath, sidebarMod);
-	
-	let module: Page = {
+	let module: Routeable = {
 		path: routePath,
 		component,
 		title,
-		// sidebar: sideComponent
+		// name: nameWithoutPage,
 	};
-	// sidebars[routePath] = sideComponent;
-	sidebars.set(nameWithoutPage, sideComponent);
+	// console.log(`Adding route:`, module);
+	foundRoutes.push(module);
 
-
-	console.log(`Adding route:`, module);
-	routes.push(module);
+	const sidebarPath = `${folder}/${nameWithoutPage}-side.ts`;
+	const sidebarMod = moduleSidebars[sidebarPath];
+	// console.log(`Looking for sidebar at:`, sidebarPath);
+	const sideComponent = getComponent(sidebarPath, sidebarMod);
+	if (sideComponent) {
+		foundSidebars.set(title, sideComponent);
+		// console.log(`Adding sidebar:`, sideComponent);
+	}
 }
